53d3c80e95556c8964147694ce4003cc8ccc5c84
Baste <assasinadyg@gmail.com>
2024-06-17 23:59:14 +0300

    New connect button

--- Assets/_Project/Scripts/MirrorRuntime/NetMan.cs
+++ Assets/_Project/Scripts/MirrorRuntime/NetMan.cs
@@ -1,9 +1,16 @@
 ﻿using Mirror;
+using UnityEngine;
 
 namespace _Project.Scripts.MirrorRuntime
 {
     public class NetMan : NetworkManager
     {
+        [SerializeField] private string _customNetworkAddress;
         
+        public void ConnectToServer()
+        {
+            networkAddress = _customNetworkAddress;
+            StartClient();
+        }
     }
 }
